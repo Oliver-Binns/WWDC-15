46c4f71c4d0a6acc0cd9d5942c90a77a956db8a7
Oliver Binns <mail@oliverbinns.info>
2015-04-26 10:13:36 +0100

    Added initial view to project

---
+++ Oliver Binns/InitialViewController.swift
@@ -0,0 +1,42 @@
+//
+//  InitialViewController.swift
+//  Oliver Binns
+//
+//  Created by Oliver on 26/04/2015.
+//  Copyright (c) 2015 Oliver Binns. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    @IBOutlet weak var meButton: UIButton!
+    @IBOutlet weak var workButton: UIButton!
+    @IBOutlet weak var musicButton: UIButton!
+    @IBOutlet weak var codeButton: UIButton!
+    @IBOutlet weak var schoolButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
